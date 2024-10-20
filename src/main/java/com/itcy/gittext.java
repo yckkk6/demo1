7e055f50f72cd689faf0a4ff00b2a646b7914690
yang <17652333962@163.com>
2024-10-20 21:31:00 +0800

    frist commite

---
+++ src/main/java/com/itcy/gittext.java
@@ -0,0 +1,8 @@
+package com.itcy;
+
+public class gittext {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+
+}
---
+++ src/main/java/controller/pageController.java
@@ -0,0 +1,5 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+

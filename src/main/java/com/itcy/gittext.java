0a8978e3c786e7b35d0f318323a39ceaf5df0187
yang <17652333962@163.com>
2024-10-21 16:52:58 +0800

    Third commite

--- src/main/java/com/itcy/gittext.java
+++ src/main/java/com/itcy/gittext.java
@@ -4,6 +4,7 @@ public class gittext {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("he two");
+        System.out.println("hot-fix three");
     }
 
 }
